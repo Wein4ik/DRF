6287449703aff808fc00c953f133c3291cb3972d
Ilya <il.stafeev@gmail.com>
2023-01-22 17:39:23 +0300

    Added serializer for model Women and view

--- women/serializers.py
+++ women/serializers.py
@@ -2,7 +2,11 @@ from rest_framework import serializers
 from .models import Women
 
 
+
+
+
 class WomenSerializer(serializers.ModelSerializer):
     class Meta:
         model = Women
-        fields = ('title', 'cat_id')
+        fields = ('title', 'content', 'cat')
+
