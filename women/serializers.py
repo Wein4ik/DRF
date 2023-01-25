0033ac791a53751fdb0c44f9a096b12775707096
Ilya <il.stafeev@gmail.com>
2023-01-25 16:27:22 +0300

    Added CRUD for model Women

--- women/serializers.py
+++ women/serializers.py
@@ -8,5 +8,5 @@ from .models import Women
 class WomenSerializer(serializers.ModelSerializer):
     class Meta:
         model = Women
-        fields = ('title', 'content', 'cat')
+        fields = "__all__"
 
