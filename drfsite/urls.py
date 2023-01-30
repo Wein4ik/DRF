fc8fd3e03592aa0b0d6f769d92302d2b3e2802c9
Ilya <il.stafeev@gmail.com>
2023-01-30 13:16:37 +0300

    Added auth with sessions and token on djoser

--- drfsite/urls.py
+++ drfsite/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework import routers
 
 from women.views import *
@@ -8,7 +8,10 @@ from women.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/drf-auth/', include('rest_framework.urls')),
     path('api/v1/women/', WomenAPIList.as_view()),
     path('api/v1/women/<int:pk>/', WomenAPIUpdate.as_view()),
     path('api/v1/womendelete/<int:pk>/', WomenAPIDestroy.as_view()),
+    path('api/v1/auth/', include('djoser.urls')),
+    re_path(r'^auth/', include('djoser.urls.authtoken')),
 ]
--- women/views.py
+++ women/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, IsAuthenticated
 
 from .models import Women, Category
 from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
@@ -15,10 +15,12 @@ class WomenAPIList(generics.ListCreateAPIView):
     serializer_class = WomenSerializer
     permission_classes = (IsAuthenticatedOrReadOnly,)
 
+
 class WomenAPIUpdate(generics.RetrieveUpdateAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
-    permission_classes = (IsOwnerOrReadOnly,)
+    permission_classes = (IsAuthenticated,)
+
 
 class WomenAPIDestroy(generics.RetrieveDestroyAPIView):
     queryset = Women.objects.all()
